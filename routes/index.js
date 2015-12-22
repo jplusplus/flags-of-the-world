a9685a309068bab0f2d6b3db318139cc7058a56d
rotsee <leo_wallentin@hotmail.com>
2015-12-22 18:10:07 +0100

    sort flag cards

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,12 @@ router.get('/', function(req, res, next) {
 
     var year =  req.query.year || 1974
     var lang = req.query.lang || "sv"
-    var api = "http://api.thenmap.net/v1/world/data/" + year + "?data_props=flag|name&data_lang=" + lang
+
+    if (req.app.get('env') === 'development') {
+        var api = "http://localhost:3000/v1/world/data/" + year + "?data_props=flag|name&data_lang=" + lang
+    } else {
+        var api = "http://api.thenmap.net/v1/world/data/" + year + "?data_props=flag|name&data_lang=" + lang
+    }
 
     http.get(api, function(reply){
         var body = ''
@@ -16,22 +21,27 @@ router.get('/', function(req, res, next) {
         })
         .on('end', function(){
             console.log(body)
-            var json = JSON.parse(body)
+            var json = JSON.parse(body)["data"]
             var data = []
-            for (var index in json["data"]){
-                if ("flag" in json["data"][index][0]){
-                    if (json["data"][index][0].flag.length > 1){
-                        console.log(json["data"][index][0].name + " needs flag quantifyers at Wikidata!")
+            for (var index in json){
+                if ("flag" in json[index][0]){
+                    if (json[index][0].flag.length > 1){
+                        console.log(json[index][0].name + " needs flag quantifyers at Wikidata!")
                     }
-                    src = json["data"][index][0].flag[0]
+                    src = json[index][0].flag[0]
                 } else {
                     src = "https://upload.wikimedia.org/wikipedia/commons/6/61/Flag.svg"
                 }
                 data.push({
                     'src': src,
-                    'name': json["data"][index][0].name
+                    'name': json[index][0].name
                 })
             }
+            data.sort(function(a, b) {
+                if(a.name < b.name) return -1
+                if(a.name > b.name) return 1
+                return 0
+            })
 
               res.render('index', { title: 'Flags of the world in ' + year,
                                     data: data 
